307eb1b3bb3a3beb02ab8e680af136af8d001763
Kairo Naser <kaironaser100@gmail.com>
2025-04-27 14:00:06 +0300

    making  function to find the price of the crytocurrency

--- crytoPricesChecker.js
+++ crytoPricesChecker.js
@@ -5,15 +5,30 @@
 const axios = require('axios');
 const readline = require('readline');
 
-// making interface for input and output
+
 const rl = readline.createInterface({
-    input: process.stdin,
+    input:process.stdin,
     output:process.stdout
-});
+})
+
 
 
 async function getCyto(price) {
-    const apikey ='CG-y6sG52uhR8FDvwEXLq4aioNp';
-    const url = `https://pro-api.coingecko.com/api/v3/ping?${price}key=${apikey}`;
+    // const apikey ='CG-y6sG52uhR8FDvwEXLq4aioNp';  in the free version i dont need one api key for coingecko
+    // const url = `https://pro-api.coingecko.com/api/v3/ping?${price}key=${apikey}`;
     
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${price}&vs_currencies=usd`;
+
+
+    try{
+        const response = await axios.get(url);
+        const cypto = response.data[price].usd;
+        //charAt retrives the first charcter of bitcoin
+        //.slice returing the rest of strings starting from second character
+        console.log(`${price.charAt(0).toUpperCase()+ price.slice(1)} Price: $${price}`)
+    }
+    catch(error){
+            console.log("error fetching cryptocurrency price ", error);
+    }
 }
+
