5d05b47382551e2b898b6fe1ef8737693c0d2fd0
Kairo Naser <kaironaser100@gmail.com>
2025-04-27 13:42:39 +0300

    importing axios and readline and create intercafe for input and output

--- crytoPricesChecker.js
+++ crytoPricesChecker.js
@@ -1,1 +1,19 @@
 // this is using CoinGecko API to change cyto prices
+
+
+// importing axios and readline module
+const axios = require('axios');
+const readline = require('readline');
+
+// making interface for input and output
+const rl = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout
+});
+
+
+async function getCyto(price) {
+    const apikey ='CG-y6sG52uhR8FDvwEXLq4aioNp';
+    const url = `https://pro-api.coingecko.com/api/v3/ping?${price}key=${apikey}`;
+    
+}
