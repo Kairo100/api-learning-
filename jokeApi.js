f98b23737b8abeb29b2f41cca84f1af054782ef5
Kairo Naser <kaironaser100@gmail.com>
2025-04-27 14:25:54 +0300

    making function to fetch joke data

--- jokeApi.js
+++ jokeApi.js
@@ -8,3 +8,25 @@ const rl = readline.createInterface({
     input:process.stdin,
     input:process.stdout
 })
+
+// function to fetch data
+async function getJoke() {
+    const url = 'https://v2.jokeapi.dev/joke/Any';
+    
+   try{
+     
+    const response = await axios.get(url);
+
+    if(response.data.type=== "single"){
+        console.log(`Joke:${response.data.joke}`);
+    }
+    else if(response.data.type=== "twopart"){
+        console.log(`Joke:${response.data.setup}`);
+        console.log(`Delivery:${response.data.delivery}`);
+    }
+   }
+   catch(error){
+    console.log('There is error fetching data from jokeApi', error);
+   }
+}
+
